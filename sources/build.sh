7e838bfe4605738248b5960aa31fb37f12e59399
Dano Williams <daniel.williams@gsa.gov>
2020-08-16 17:20:48 -0700

    Fix display and directory bugs in build script

--- sources/build.sh
+++ sources/build.sh
@@ -9,42 +9,43 @@ axis="wght" #eg with multiple axis "wdth,wght" --> with comma, no space
 #===========================================================================
 #Generating fonts ==========================================================
 
-source ../env/bin/activate
+source env/bin/activate
 set -e
 
-echo "BUILDING PUBLIC SANS
-|"
-echo "CLEANING FONTS FOLDERS
-|"
-#rm -rf ./fonts/ttf/ ./fonts/otf/ ./fonts/variable/ ./fonts/webfonts/
+echo "│  █▄▄ █░█ █ █░░ █▀▄   █▀█ █░█ █▄▄ █░░ █ █▀▀   █▀ ▄▀█ █▄░█ █▀
+│  █▄█ █▄█ █ █▄▄ █▄▀   █▀▀ █▄█ █▄█ █▄▄ █ █▄▄   ▄█ █▀█ █░▀█ ▄█
+│"
+echo "├─ Cleaning font directories
+│"
+rm -rf ./fonts/ttf/ ./fonts/otf/ ./fonts/variable/ ./fonts/webfonts/
 rm -rf ./master_ufo/ ./instance_ufo/
 
-echo "GENERATING STATIC TTF
-|"
+echo "├─ Generating static OTF
+│"
 mkdir -p ./fonts/ttf
 fontmake -g ./sources/$thisFont.glyphs -i -o ttf --output-dir ./fonts/ttf/
-fontmake -g ./sources/$thisFont-italics.glyphs -i -o ttf --output-dir ./fonts/ttf/
+fontmake -g ./sources/$thisFont-Italic.glyphs -i -o ttf --output-dir ./fonts/ttf/
 
-echo "|
-GENERATING STATIC OTF
-|"
+echo "│
+├─ Generating OTF
+│"
 mkdir -p ./fonts/otf
 fontmake -g ./sources/$thisFont.glyphs -i -o otf --output-dir ./fonts/otf/
-fontmake -g ./sources/$thisFont-italics.glyphs -i -o otf --output-dir ./fonts/otf/
+fontmake -g ./sources/$thisFont-Italic.glyphs -i -o otf --output-dir ./fonts/otf/
 
-echo "|
-GENERATING VARIABLE FONTS
-|"
+echo "│
+├─ Generating variable fonts
+│"
 mkdir -p ./fonts/variable
 fontmake -g ./sources/$thisFont.glyphs -o variable --output-path ./fonts/variable/$thisFont\[wght\].ttf
-fontmake -g ./sources/$thisFont-italics.glyphs -o variable --output-path ./fonts/variable/$thisFont-Italic\[wght\].ttf
+fontmake -g ./sources/$thisFont-Italic.glyphs -o variable --output-path ./fonts/variable/$thisFont-Italic\[wght\].ttf
 
 #============================================================================
 #Post-processing fonts ======================================================
 
-echo "|
-POST-PROCESSING TTF
-|"
+echo "│
+├─ Post-processing TTF
+│"
 ttfs=$(ls ./fonts/ttf/*.ttf)
 echo $ttfs
 for ttf in $ttfs
@@ -56,9 +57,9 @@ do
 	[ -f $ttf.fix ] && mv $ttf.fix $ttf
 done
 
-echo "|
-POST-PROCESSING OTF
-|"
+echo "│
+├─ Post-processing OTF
+│"
 otfs=$(ls ./fonts/otf/*.otf)
 for otf in $otfs
 do
@@ -67,9 +68,9 @@ do
 	[ -f $otf.fix ] && mv $otf.fix $otf
 done
 
-echo "|
-POST-PROCESSING VF
-|"
+echo "│
+├─ Post-processing variable fonts
+│"
 vfs=$(ls ./fonts/variable/*.ttf)
 for vf in $vfs
 do
@@ -77,47 +78,48 @@ do
 	gftools fix-nonhinting $vf $vf.fix
 	mv $vf.fix $vf
 	gftools fix-unwanted-tables --tables MVAR $vf
-done
-rm ../fonts/variable/*gasp*
-
-gftools fix-vf-meta $VF_FILE $VF_FILE_IT
-for vf in $vfs
-do
+	gftools fix-vf-meta $vf
 	mv $vf.fix $vf
 done
+rm ./fonts/variable/*gasp*
 
 #============================================================================
 #Build woff and woff2 fonts =================================================
 #requires https://github.com/bramstein/homebrew-webfonttools
 
-echo "|
-BUILDING WEBFONTS
-|"
+echo "│
+├─ Building webfonts
+│"
 mkdir -p ./fonts/webfonts
 
-ttfs=$(ls ../fonts/ttf/*.ttf)
+ttfs=$(ls ./fonts/ttf/*.ttf)
 for ttf in $ttfs
 do
   woff2_compress $ttf
   sfnt2woff-zopfli $ttf
 done
 
-woffs=$(ls ../fonts/ttf/*.woff*)
+woffs=$(ls ./fonts/ttf/*.woff*)
 for woff in $woffs
 do
-	mv $woff ../fonts/webfonts/
+	mv $woff ./fonts/webfonts/
 done
 
-echo "|
-SYNCING UFO/DESIGNSPACE TO SOURCES DIRECTORY
-|"
+echo "│
+├─ Syncing ufo/designspace to /sources directory
+│"
 rsync -avzhr ./master_ufo/ ./sources/ufo/
 
-echo "|
-CLEANING UP
-|"
+echo "│
+├─ Cleaning up
+│"
 rm -rf ./master_ufo/ ./instance_ufo/
 
-echo "|
-COMPLETE!
+echo "│
+└─ ░██████╗██╗░░░██╗░█████╗░░█████╗░███████╗░██████╗░██████╗██╗
+   ██╔════╝██║░░░██║██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝██║
+   ╚█████╗░██║░░░██║██║░░╚═╝██║░░╚═╝█████╗░░╚█████╗░╚█████╗░██║
+   ░╚═══██╗██║░░░██║██║░░██╗██║░░██╗██╔══╝░░░╚═══██╗░╚═══██╗╚═╝
+   ██████╔╝╚██████╔╝╚█████╔╝╚█████╔╝███████╗██████╔╝██████╔╝██╗
+   ╚═════╝░░╚═════╝░░╚════╝░░╚════╝░╚══════╝╚═════╝░╚═════╝░╚═╝
 "
