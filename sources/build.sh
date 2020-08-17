5ea7fe0c672de4ed695ddadf75e16f1f496df289
Dano Williams <daniel.williams@gsa.gov>
2020-08-16 20:36:03 -0700

    Comment out weightclass fix
    This was failing locally. For some reason it was unable to parse itlic fonts

--- sources/build.sh
+++ sources/build.sh
@@ -64,8 +64,8 @@ otfs=$(ls ./fonts/otf/*.otf)
 for otf in $otfs
 do
 	gftools fix-dsig -f $otf
-	gftools fix-weightclass $otf
-	[ -f $otf.fix ] && mv $otf.fix $otf
+	#gftools fix-weightclass $otf
+	#[ -f $otf.fix ] && mv $otf.fix $otf
 done
 
 echo "│
