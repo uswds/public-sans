614dca823deb5ec172b037c842df0c7f8e9c3848
gscottqueen <geoffreysqueen@gmail.com>
2021-08-17 22:21:47 -0400

    mounting the local volume to the working directory

--- build-font.sh
+++ build-font.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t public_sans_temp .
-docker run -v $(pwd):/usr/src/uswds public_sans_temp
+docker build -t public_sans_temp . --progress=plain
+docker run --mount type=bind,source="$(pwd)",target=/public_sans public_sans_temp
